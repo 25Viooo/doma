db3e8c56e05562e90705bf5af83adb873551f927
25Viooo <147171101+25Viooo@users.noreply.github.com>
2023-10-06 21:02:45 +0500

    Update clcltr.java

--- clcltr.java
+++ clcltr.java
@@ -4,36 +4,30 @@ public class clcltr{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        //Ввод первого числа
         System.out.print("Введите первое число: ");
         double number1 = scanner.nextDouble();
-        //Ввод второго числа
         System.out.print("Введите второе число: ");
         double number2 = scanner.nextDouble();
-        //Ввод оператора
         System.out.print("Введите оператор (+, -, *, /): ");
         char operator = scanner.next().charAt(0);
-        //переменная, содержащая ответ
         double result = 0;
-        //условный метод, выводящий нужную операцию
-        switch (operator) { //начало метода
-            case '+'://Что будет делаться если ввели "+"
+        switch (operator) { 
+            case '+':
                 result = number1 + number2;
                 break;
-            case '-'://Что будет делаться если ввели "-"
+            case '-':
                 result = number1 - number2;
                 break;
-            case '*'://Что будет делаться если ввели "*"
+            case '*':
                 result = number1 * number2;
                 break;
-            case '/'://Что будет делаться если ввели "/"
+            case '/':
                 result = number1 / number2;
                 break;
-            default://Что будет делаться если ввели неверный оператор
+            default:
                 System.out.println("Некорректный оператор!");
                 return;
         }
-        //Вывод результата
         System.out.println("Результат: " + result);
     }
 }
